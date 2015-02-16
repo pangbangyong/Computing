103c2d2a7adef7f7be9a4e39f96aa95868794d01
pangbangyong <pang.bangyong@dhs.sg>
2015-02-17 01:19:37 +0800

    Create q2_calc_cylinder_volume.py

---
+++ q2_calc_cylinder_volume.py
@@ -0,0 +1,8 @@
+from math import pi
+
+radius = float(input('Please key in the radius of the cyclinder'))
+length = float(input('Please key in the length of the cylinder'))
+
+volume = radius * radius * pi * length
+
+print('The volume of the cylinder is:', volume)
